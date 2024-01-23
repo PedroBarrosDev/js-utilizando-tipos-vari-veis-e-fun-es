a39fe235a7d1074b4a40d7b399940954be760c97
Pedro Barros <devpedrobarros@hotmail.com>
2024-01-22 23:36:26 -0300

    tipos de dados

---
+++ tipo-string.js
@@ -0,0 +1,12 @@
+const sobrenome = "Barros"
+const nome = `Pedro ${sobrenome}`
+
+const senha = 'SenhaSegura123' + nome.toUpperCase();
+
+console.log(senha)
+console.log(nome)
+
+const teste = "8.1"
+console.log(Number(teste))
+const teste2 = "8.9"
+console.log(parseFloat(teste2))
